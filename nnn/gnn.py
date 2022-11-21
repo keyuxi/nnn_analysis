fe6a1d878b177d9585e2183984f767094d152d49
keyuxi <keyuxi@yeah.net>
2022-11-21 11:58:42 -0800

    update ECL analysis

--- nnn/gnn.py
+++ nnn/gnn.py
@@ -1,11 +1,174 @@
+
 """
 Define the training pipeline
 """
-from torch.nn import Linear, BatchNorm1d, ReLU, Sequential, ModuleList
+import os
+import torch
+os.environ['TORCH'] = torch.__version__
+import numpy as np
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json, os, sys
+import sklearn
+from scipy.stats import pearsonr
+from sklearn.metrics import r2_score
+
+from torch_geometric.data import InMemoryDataset, Data
+from torch_geometric.loader import DataLoader
+import wandb
+
+kB = 0.0019872 # Bolzman constant
+C2T = 273.15 # conversion from celsius to kalvin
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+from pandas.core.frame import properties
+from torch.nn import Linear, BatchNorm1d, Sequential, ModuleList
+from torch import nn
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv, GINConv, GATv2Conv, BatchNorm, TransformerConv
 from torch_geometric.nn import global_mean_pool, global_add_pool
 from torch_geometric.nn.aggr import Set2Set
+
+
+def dotbracket2edgelist(dotbracket_str:str, 
+                        edge_feature:bool=True):
+    
+    assert isinstance(dotbracket_str, str), f'{dotbracket_str} is not a string'
+    assert dotbracket_str.count('(') == dotbracket_str.count(')'), \
+        'Number of "(" and ")" should match in %s' % dotbracket_str
+
+    # Backbone edges
+    N = len(dotbracket_str)
+    edge_5p_list = [[i, i+1] for i in range(N-1)]
+
+    # Hydrogen bonds
+    edge_hbond_list = []
+    flag3p = N - 1
+    for i,x in enumerate(dotbracket_str):
+        if x == '(':
+            for j in range(flag3p, i, -1):
+                if dotbracket_str[j] == ')':
+                    edge_hbond_list.append([i, j])
+                    flag3p = j - 1
+                    break
+
+    # 5to3, 3to5, bidirectional hbond
+    edge_list = edge_5p_list + [e[::-1] for e in edge_5p_list] + edge_hbond_list + [e[::-1] for e in edge_hbond_list]
+    
+    if edge_feature:
+        n_backbone, n_hbond = len(edge_5p_list), len(edge_hbond_list)
+        edge_attr = np.zeros((len(edge_list), 3), dtype=int)
+        edge_attr[:n_backbone, 0] = 1
+        edge_attr[n_backbone:n_backbone*2, 1] = 1
+        edge_attr[-2 * n_hbond:, 2] = 1
+        return edge_list, edge_attr
+    else:
+        return edge_list
+
+
+def onehot_nucleotide(seq_str):
+    map_dict = dict(A=0, T=1, C=2, G=3)
+    N = len(seq_str)
+    encode_arr = np.zeros((N, 4))
+    for i,x in enumerate(seq_str.upper()):
+        encode_arr[i, map_dict[x]] = 1
+    return encode_arr
+
+def norm_dH(dH):
+    return (dH + 26) / 10
+
+def unorm_dH(dH_norm):
+    return dH_norm * 10 - 26
+
+def norm_Tm(Tm):
+    return (Tm - 40) / 7
+
+def unorm_Tm(Tm_norm):
+    return Tm_norm * 7 + 40
+
+def row2graphdata(row):
+    edge_list, edge_feat = dotbracket2edgelist(row['TargetStruct'])
+    edge_index = torch.tensor(np.array(edge_list).T, dtype=torch.long) #int64
+    edge_attr = torch.tensor(edge_feat, dtype=torch.float)
+    x = torch.tensor(onehot_nucleotide(row['RefSeq']), dtype=torch.float)
+    y = torch.tensor([norm_dH(row['dH']), norm_Tm(row['Tm'])], dtype=torch.float)
+
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
+    return data
+
+assert np.allclose(onehot_nucleotide('AT'), np.array([[1, 0, 0, 0], [0, 1, 0, 0]]))
+
+class NNNDataset(InMemoryDataset):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+        with open(os.path.join(self.raw_dir, self.raw_file_names[1]), 'r') as fh:
+            self.data_split_dict = json.load(fh)
+
+        self.arr = pd.read_csv(os.path.join(self.raw_dir, self.raw_file_names[0]), index_col=0)
+        self.seqid = self.arr.index
+
+    @property
+    def raw_file_names(self):
+        return ['arr.csv', 'train_val_test_split.json']
+
+    @property
+    def processed_file_names(self):
+        return ['data.pt']
+
+    @property
+    def train_set(self):
+        ind = np.searchsorted(self.seqid, 
+                                    self.data_split_dict['train_ind'])
+        return self.index_select(ind)
+
+    @property
+    def val_set(self):
+        ind = np.searchsorted(self.seqid, 
+                              self.data_split_dict['val_ind'])
+        return self.index_select(ind)
+
+    @property
+    def test_set(self):
+        ind = np.searchsorted(self.seqid, 
+                              self.data_split_dict['test_ind'])
+        return self.index_select(ind)
+
+    def process(self):
+        print(self.raw_dir)
+        self.arr = pd.read_csv(os.path.join(self.raw_dir, self.raw_file_names[0]), index_col=0)
+        data_list = [row2graphdata(row) for _,row in self.arr.iterrows()]
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+
+class NNNDatasetCorrected(NNNDataset):
+    @property
+    def raw_file_names(self):
+        return ['arr_corrected_inplace.csv', 'train_val_test_split.json']
+    
+
+def sweep_model():
+    with wandb.init() as run:
+        config = wandb.config
+        # make the model, data, and optimization problem
+        model, train_loader, test_loader, criterion, optimizer = make(config)
+
+        # and use them to train the model
+        train(model, train_loader, test_loader, criterion, optimizer, config)
+
+        # and test its final performance
+        test(model, train_loader, test_loader)
 
 def model_pipeline(hyperparameters):
 
@@ -31,7 +194,9 @@ def make(config):
     # Make the data
     torch.manual_seed(12345)
     if config['dataset'] == 'NNN_v0':
-        dataset = NNNDataset(root='/content/drive/My Drive/Colab Notebooks/data/nnn')
+        dataset = NNNDataset(root='/mnt/d/data/nnn/')
+    elif config['dataset'] == 'NNN_v0.1':
+        dataset = NNNDatasetCorrected(root='/mnt/d/data/nnn/')
     has_gpu = torch.cuda.is_available()
     train_loader = DataLoader(dataset.train_set, batch_size=config['batch_size'],
                             shuffle=True, pin_memory=has_gpu)
@@ -41,11 +206,15 @@ def make(config):
     # Make the model
     if config['architecture'] == 'GraphTransformer':
         model = GTransformer(config).to(device)
+    # elif config['architecture'] == 'GAT':
+    #     model = GAT(config['hidden_channels']).to(device)
+    else:
+        raise 'Check `architecture` in config dictionary!'
 
     # Make the loss and optimizer
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(
-        model.parameters(), lr=config'learning_rate'])
+        model.parameters(), lr=config['learning_rate'])
     
     return model, train_loader, test_loader, criterion, optimizer
 
@@ -61,27 +230,33 @@ class GTransformer(torch.nn.Module):
         self.graphconv_dropout = config['graphconv_dropout']
         self.linear_dropout = config['linear_dropout']
 
-        self.convs = ModuleList([
+        conv_list = ([
             TransformerConv(num_node_features, config['hidden_channels'],
-                               heads=num_heads, edge_dim=num_edge_features)] +
+                               heads=num_heads, edge_dim=num_edge_features, dropout=self.graphconv_dropout)] +
             [TransformerConv(config['hidden_channels'], config['hidden_channels'],
-                               heads=num_heads, edge_dim=num_edge_features)] *
+                               heads=num_heads, edge_dim=num_edge_features, dropout=self.graphconv_dropout)] *
             (config['n_graphconv_layer'] - 1))
+        self.convs = ModuleList(conv_list)
 
-        self.norm = BatchNorm(in_channels=config['hidden_channels'])
+        # self.norm = BatchNorm(in_channels=config['hidden_channels'])
 
         linear_list = []
+        self.concat = config['concat']
+        if config['concat']:
+          n_pool = config['hidden_channels'] * config['n_graphconv_layer']
+        else:
+          n_pool = config['hidden_channels']
         if config['pooling'] == 'Set2Set':
-            self.aggr = Set2Set(config['hidden_channels'], 
+            self.aggr = Set2Set(n_pool, 
                                 processing_steps=config['processing_steps'])
-            linear_list.append(Linear(2*config['hidden_channels'], config['linear_hidden_channels'][0]))
+            linear_list.append(Linear(2*n_pool, config['linear_hidden_channels'][0]))
         elif config['pooling'] == 'global_add_pool':
             self.aggr = global_add_pool
             linear_list.append(Linear(config['hidden_channels'], config['linear_hidden_channels'][0]))
         else:
             raise 'Invalid config.pooling %s' % config['pooling']
         
-        if config['n_linear_layer'] > 2:
+        if config['n_linear_layer'] >= 2:
             linear_list.extend([Linear(config['linear_hidden_channels'][i - 1], 
                                     config['linear_hidden_channels'][i])
                                 for i in range(1, config['n_linear_layer'])])
@@ -89,27 +264,40 @@ class GTransformer(torch.nn.Module):
                                   num_params))
         self.linears = ModuleList(linear_list)
 
+        # self.trace = []
+
     def forward(self, x, edge_index, edge_attr, batch):
+        self.trace = []
         # 1. Obtain node embeddings
         for i, l in enumerate(self.convs):
             x = l(x, edge_index, edge_attr)
+            self.trace.append(x)
             x = F.leaky_relu(x)
             x = F.dropout(x, p=self.graphconv_dropout, training=self.training)
 
         # 2. Pooling layer
+        if self.concat:
+          x = torch.cat(self.trace, dim=1)
         x = self.aggr(x, batch)
 
         # 3. Apply a final regressor
         for i, l in enumerate(self.linears):
             x = l(x)
-            x = F.relu(x)
             if i < len(self.linears) - 1:
+                x = F.relu(x)
                 x = F.dropout(x, p=self.linear_dropout, training=self.training)
 
         x = torch.flatten(x)
         
         return x
 
+    @property
+    def n_parameters(self):
+        n_param = 0
+        for param in model.parameters():
+            n_param += np.prod(np.array(param.shape))
+
+        print(n_param)
 
 def train_epoch(model, train_loader, criterion, optimizer, config):
     """
@@ -216,7 +404,14 @@ def train(model, train_loader, test_loader, criterion, optimizer, config):
             print(f'Epoch: {epoch:03d}, Train RMSE: {train_rmse:.4f}, Test RMSE: {test_rmse:.4f}')
 
 
+def get_n_param(model):
+    n_param = 0
+    for param in model.parameters():
+        n_param += np.prod(np.array(param.shape))
+    return n_param
+
 def test(model, train_loader, test_loader):
+
     train_result = get_truth_pred(train_loader, model)
     test_result = get_truth_pred(test_loader, model)
 
@@ -225,10 +420,11 @@ def test(model, train_loader, test_loader):
     _ = plot_truth_pred(train_result, ax[0,1], param='Tm')
     dH_rmse, dH_mae = plot_truth_pred(test_result, ax[1,0], param='dH', title='Validation')
     Tm_rmse, Tm_mae = plot_truth_pred(test_result, ax[1,1], param='Tm', title='Validation')
-
+    wandb.log({'fig': fig})
     wandb.run.summary["dH_rmse"] = dH_rmse
     wandb.run.summary["dH_mae"] = dH_mae
     wandb.run.summary["Tm_rmse"] = Tm_rmse
     wandb.run.summary["Tm_mae"] = Tm_mae
+    wandb.run.summary["n_parameters"] = get_n_param(model)
 
     plt.show()
